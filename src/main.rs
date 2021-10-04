2c459da55ca4f94f23d01913dc5ee3c3b84d48f4
Yaroslav Khodakovskij <wwwinche@gmail.com>
2021-10-04 21:19:05 +0300

    Format file and rework to rand and file iteration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use chess::{Color, Figure, Game, Tile};
-use iced::{
-    pane_grid, Align, Button, Column, Container, Element, Image, Length, PaneGrid, Row, Sandbox,
-    Settings, Text,
-};
+use chess::{Color, FieldTrait, Figure, File, Game, Position, Rank, Tile};
+use iced::{Column, Container, Element, Image, Length, Row, Sandbox, Settings};
 
 pub fn main() -> iced::Result {
     ChessApp::run(Settings::default())
@@ -10,16 +7,6 @@ pub fn main() -> iced::Result {
 
 struct ChessApp {
     game: Game,
-    panes: pane_grid::State<Content>,
-}
-
-#[derive(Debug, Clone, Copy, Default)]
-struct Content();
-
-impl Content {
-    fn new() -> Content {
-        Content {}
-    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -29,10 +16,7 @@ impl Sandbox for ChessApp {
     type Message = Message;
 
     fn new() -> Self {
-        ChessApp {
-            game: Game::new(),
-            panes: pane_grid::State::new(Content::new()).0,
-        }
+        ChessApp { game: Game::new() }
     }
 
     fn title(&self) -> String {
@@ -43,23 +27,17 @@ impl Sandbox for ChessApp {
 
     fn view(&mut self) -> Element<Message> {
         let mut content = Column::new();
-        for (column, column_tiles) in self.game.field.iter().enumerate() {
-            let mut row_element = Row::new();
-            for (row, tile) in column_tiles.iter().enumerate() {
-                let image = get_image(tile).width(Length::Fill).height(Length::Fill);
-                let is_even = (column + row) % 2 == 0;
-                let tile_style = if is_even {
-                    style::Tile::Even
-                } else {
-                    style::Tile::Odd
+        for rank in Rank::iter().rev() {
+            let mut rank_elements = Row::new();
+            for file in File::iter() {
+                let position = Position {
+                    rank: *rank,
+                    file: *file,
                 };
-                let container = Container::new(image)
-                    .width(Length::Units(40))
-                    .height(Length::Units(40))
-                    .style(tile_style);
-                row_element = row_element.push(container);
+                let element = self.get_tile_element(position);
+                rank_elements = rank_elements.push(element);
             }
-            content = content.push(row_element);
+            content = content.push(rank_elements);
         }
 
         Container::new(content)
@@ -71,25 +49,44 @@ impl Sandbox for ChessApp {
     }
 }
 
-fn get_image(tile: &Tile) -> Image {
-    let path = match tile {
-        Tile::Occupied(color, figure) => match (color, figure) {
-            (Color::White, Figure::Pawn) => "images/pawn_white.png",
-            (Color::White, Figure::Knight) => "images/knight_white.png",
-            (Color::White, Figure::Bishop) => "images/bishop_white.png",
-            (Color::White, Figure::Rook) => "images/rook_white.png",
-            (Color::White, Figure::Queen) => "images/queen_white.png",
-            (Color::White, Figure::King) => "images/king_white.png",
-            (Color::Black, Figure::Pawn) => "images/pawn_black.png",
-            (Color::Black, Figure::Knight) => "images/knight_black.png",
-            (Color::Black, Figure::Bishop) => "images/bishop_black.png",
-            (Color::Black, Figure::Rook) => "images/rook_black.png",
-            (Color::Black, Figure::Queen) => "images/queen_black.png",
-            (Color::Black, Figure::King) => "images/king_black.png",
-        },
-        _ => "",
-    };
-    Image::new(path)
+impl ChessApp {
+    fn get_tile_element(&self, position: Position) -> Container<Message> {
+        let tile = self.game.field.get(position);
+        let image = ChessApp::get_tile_image(&tile)
+            .width(Length::Fill)
+            .height(Length::Fill);
+        let is_even = (position.rank as usize + position.file as usize) % 2 == 0;
+        let tile_style = if is_even {
+            style::Tile::Even
+        } else {
+            style::Tile::Odd
+        };
+        Container::new(image)
+            .width(Length::Units(40))
+            .height(Length::Units(40))
+            .style(tile_style)
+    }
+
+    fn get_tile_image(tile: &Tile) -> Image {
+        let path = match tile {
+            Tile::Occupied(color, figure) => match (color, figure) {
+                (Color::White, Figure::Pawn) => "images/pawn_white.png",
+                (Color::White, Figure::Knight) => "images/knight_white.png",
+                (Color::White, Figure::Bishop) => "images/bishop_white.png",
+                (Color::White, Figure::Rook) => "images/rook_white.png",
+                (Color::White, Figure::Queen) => "images/queen_white.png",
+                (Color::White, Figure::King) => "images/king_white.png",
+                (Color::Black, Figure::Pawn) => "images/pawn_black.png",
+                (Color::Black, Figure::Knight) => "images/knight_black.png",
+                (Color::Black, Figure::Bishop) => "images/bishop_black.png",
+                (Color::Black, Figure::Rook) => "images/rook_black.png",
+                (Color::Black, Figure::Queen) => "images/queen_black.png",
+                (Color::Black, Figure::King) => "images/king_black.png",
+            },
+            _ => "",
+        };
+        Image::new(path)
+    }
 }
 
 mod style {
